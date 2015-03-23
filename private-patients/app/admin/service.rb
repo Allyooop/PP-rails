7fb2de75c4ef18fdf52200e0396bece75bf1ef97
GrilloPress <grillopress@gmail.com>
2015-03-23 22:52:29 +0000

    add service model to admin panel

---
+++ private-patients/app/admin/service.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Service do
+
+  # string   "title"
+  # string   "department"
+  # string   "specialty"
+  # text     "body"
+  # string   "banner"
+  # integer  "metatag_id"
+  # text     "sidebar"
+
+  # as with Rails controllers, white list actions
+  permit_params :title, :body, :department, :specialty, :banner, :sidebar, :metatag_id
+  
+  # enable the right-hand sidebar to search and filter on certain fields
+  filter :title
+  filter :department
+  filter :specialty
+
+end
--- private-patients/app/models/service.rb
+++ private-patients/app/models/service.rb
@@ -1,3 +1,12 @@
 class Service < ActiveRecord::Base
+  
+  # string   "title"
+  # string   "department"
+  # string   "specialty"
+  # text     "body"
+  # string   "banner"
+  # integer  "metatag_id"
+  # text     "sidebar"
+  
   has_one :metatag
 end
