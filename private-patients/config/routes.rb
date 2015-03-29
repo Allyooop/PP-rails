45112e35ab031bcdb16392989b5dc858e2bfb17a
GrilloPress <grillopress@gmail.com>
2015-03-29 19:41:49 +0000

    remove additional images from front page

--- private-patients/config/routes.rb
+++ private-patients/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   
   resources :services, only: [:index, :show]
-
   resources :contacts, only: [ :index, :new, :create]
 
   devise_for :admins, ActiveAdmin::Devise.config
