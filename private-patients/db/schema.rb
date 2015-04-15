f59669ae995b14b8ad5b4a221c10b50755bbcc53
GrilloPress <grillopress@gmail.com>
2015-04-15 14:33:17 +0000

    move to local mysql db

--- private-patients/db/schema.rb
+++ private-patients/db/schema.rb
@@ -14,83 +14,87 @@
 ActiveRecord::Schema.define(version: 20150414141845) do
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
+    t.string   "namespace",     limit: 255
+    t.text     "body",          limit: 65535
+    t.string   "resource_id",   limit: 255,   null: false
+    t.string   "resource_type", limit: 255,   null: false
+    t.integer  "author_id",     limit: 4
+    t.string   "author_type",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.text     "message",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "metatags", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "keywords"
-    t.string   "author"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "service_id"
+    t.string   "title",       limit: 255
+    t.string   "description", limit: 255
+    t.string   "keywords",    limit: 255
+    t.string   "author",      limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "service_id",  limit: 4
   end
 
-  add_index "metatags", ["service_id"], name: "index_metatags_on_service_id"
+  add_index "metatags", ["service_id"], name: "index_metatags_on_service_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "priority",   default: 0
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "service_id"
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
+    t.integer  "priority",   limit: 4,     default: 0
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.integer  "service_id", limit: 4
   end
 
-  add_index "questions", ["service_id"], name: "index_questions_on_service_id"
+  add_index "questions", ["service_id"], name: "index_questions_on_service_id", using: :btree
 
   create_table "services", force: :cascade do |t|
-    t.string   "title"
-    t.string   "department"
-    t.string   "specialty"
-    t.text     "body"
-    t.string   "banner"
-    t.integer  "metatag_id"
-    t.text     "sidebar"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "question_id"
+    t.string   "title",       limit: 255
+    t.string   "department",  limit: 255
+    t.string   "specialty",   limit: 255
+    t.text     "body",        limit: 65535
+    t.string   "banner",      limit: 255
+    t.integer  "metatag_id",  limit: 4
+    t.text     "sidebar",     limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "question_id", limit: 4
   end
 
-  add_index "services", ["metatag_id"], name: "index_services_on_metatag_id"
-  add_index "services", ["question_id"], name: "index_services_on_question_id"
+  add_index "services", ["metatag_id"], name: "index_services_on_metatag_id", using: :btree
+  add_index "services", ["question_id"], name: "index_services_on_question_id", using: :btree
 
+  add_foreign_key "metatags", "services"
+  add_foreign_key "questions", "services"
+  add_foreign_key "services", "metatags"
+  add_foreign_key "services", "questions"
 end
