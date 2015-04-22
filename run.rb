db54345215787ce5f58c45d17cf4bfd476aab965
GrilloPress <grillopress@gmail.com>
2015-04-22 13:40:31 +0000

    add jquery turbolink gem

--- run.rb
+++ run.rb
@@ -1,4 +1,1 @@
-puts `parts start mysql`
-puts "mysql now started..."
-puts `cd private-patients && rails s -b 0.0.0.0`
-puts "rails is running"
+puts `parts start mysql && cd private-patients && rails s -b 0.0.0.0`
